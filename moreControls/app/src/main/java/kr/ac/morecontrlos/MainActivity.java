b8533c81e6c1bc4db516ba043f1ea884ce0c2776
minwoo2500 <42472602+Yoon0511@users.noreply.github.com>
2022-03-23 12:57:46 +0900

    add moreControls

---
+++ moreControls/app/src/main/java/kr/ac/morecontrlos/MainActivity.java
@@ -0,0 +1,30 @@
+package kr.ac.morecontrlos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private CheckBox checkbox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkbox = findViewById(R.id.checkbox);
+    }
+
+    public void onCheckbox(View view) {
+        CheckBox cb = (CheckBox) view;
+        Log.d(TAG,"Checkbox state: " + cb.isChecked());
+    }
+
+    public void onBtnDoIt(View view) {
+        Log.d(TAG,"onBtnDoIt, check" + checkbox.isChecked());
+    }
+}
