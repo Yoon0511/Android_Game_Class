a18dbafd8692be24cbf4db368584dfcf5315ed30
minwoo2500 <42472602+Yoon0511@users.noreply.github.com>
2022-04-12 16:23:32 +0900

    타워디펜스 타일추가

---
+++ TowerDefence/app/src/main/java/kr/ac/my_towerdefence/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.my_towerdefence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.ac.my_towerdefence.framework.GameView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      
+    }
+}
---
+++ TowerDefence/app/src/main/java/kr/ac/my_towerdefence/game/Tile.java
@@ -0,0 +1,15 @@
+package kr.ac.my_towerdefence.game;
+
+import kr.ac.my_towerdefence.framework.Sprite;
+
+public class Tile extends Sprite {
+    protected int roadOrder;
+    public Tile(float x, float y, float w, float h, int bitmapResId,int order) {
+        super(x, y, w, h, bitmapResId);
+        this.roadOrder = order;
+    }
+
+    public int getRoadOrder(){
+        return roadOrder;
+    }
+}
