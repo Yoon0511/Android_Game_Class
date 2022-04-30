e03e4092607add7b7a3bf5f0a4600a3d2060a865
minwoo2500 <42472602+Yoon0511@users.noreply.github.com>
2022-04-30 22:11:39 +0900

    타워디펜스 object 재활용

---
+++ TowerDefence/app/src/main/java/kr/ac/my_towerdefence/framework/AnimSprite.java
@@ -0,0 +1,42 @@
+package kr.ac.my_towerdefence.framework;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+public class AnimSprite extends Sprite {
+    protected final int imageWidth;
+    protected final int imageHeight;
+    protected final int frameCount;
+    private final float framesPerSecond;
+    private Rect srcRect = new Rect();
+//    private float time;
+    private long createdOn;
+
+    public AnimSprite(float x, float y, float w, float h, int bitmapResId, float framesPerSecond, int frameCount) {
+        super(x, y, w, h, bitmapResId);
+
+        imageHeight = bitmap.getHeight();
+        if (frameCount == 0) {
+            imageWidth = imageHeight;
+            frameCount = bitmap.getWidth() / imageHeight;
+        } else {
+            imageWidth = bitmap.getWidth() / frameCount;
+        }
+
+        this.framesPerSecond = framesPerSecond;
+        this.frameCount = frameCount;
+        srcRect.set(0, 0, imageWidth, imageHeight);
+
+        createdOn = System.currentTimeMillis();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        long now = System.currentTimeMillis();
+        float time = (now - createdOn) / 1000.0f;
+        int frameIndex = Math.round(time * framesPerSecond) % frameCount;
+        srcRect.set(frameIndex * imageWidth, 0,
+                (frameIndex + 1) * imageWidth, imageHeight);
+        canvas.drawBitmap(bitmap, srcRect, dstRect, null);
+    }
+}
---
+++ TowerDefence/app/src/main/java/kr/ac/my_towerdefence/framework/Recyclable.java
@@ -0,0 +1,5 @@
+package kr.ac.my_towerdefence.framework;
+
+public interface Recyclable {
+    public void finish();
+}
